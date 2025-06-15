dfd8c98cb1d9362f039caeb5589c003618965b6d
Randy-creator <hei.randy.2@gmail.com>
2025-06-15 09:39:19 +0300

    chore: initialize spring boot project template

---
+++ src/test/java/org/template/iazz/IazzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.template.iazz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IazzApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
